9747e521f7527c93a9b19fdac3d95a112544eb4e
tereschenko.eugene@gmail.com <tereschenko.eugene@gmail.com>
2021-03-30 22:10:01 +0300

    first commit

---
+++ src/main/java/io/javabrains/securingsecurityjwt/SecuringSecurityJwtApplication.java
@@ -0,0 +1,13 @@
+package io.javabrains.securingsecurityjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecuringSecurityJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecuringSecurityJwtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/javabrains/securingsecurityjwt/SecuringSecurityJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package io.javabrains.securingsecurityjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecuringSecurityJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
